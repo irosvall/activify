760be9ceb652df9762c192d8bfbd0ed8bca813b6
Viktor Rosvall <virosen@yahoo.se>
2021-09-10 23:07:13 +0200

    Added google material to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityCardComponent } from './components/activity-card/activity-card.component';
+import { MatCardModule } from "@angular/material/card";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ActivityCardComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		MatCardModule,
+		BrowserAnimationsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
