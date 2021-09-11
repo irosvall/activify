31d730b9efe5096964f5b22f8132f64da0b9879c
irosvall <idarosvall1@gmail.com>
2021-09-11 10:17:32 +0200

    Add activity component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { ActivityCardComponent } from './components/activity-card/activity-card.component';
 import { MatCardModule } from "@angular/material/card";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from "@angular/material/icon";
+import { ActivityComponent } from './components/activity/activity.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ActivityCardComponent,
+  ActivityComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		MatCardModule,
 		BrowserAnimationsModule,
+		MatIconModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/components/activity/activity.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Activity } from "../../models/activity";
+
+@Component({
+  selector: 'app-activity',
+  templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.css']
+})
+export class ActivityComponent implements OnInit {
+  public activity: Activity;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
